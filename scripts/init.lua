2db25f48a6f14ef84943ee4a8868bc9c82149cae
NTumminaro <nicholastumminaro@gmail.com>
2023-03-09 03:53:13 -0600

    initial commit

---
+++ scripts/init.lua
@@ -0,0 +1,6 @@
+ScriptHost:LoadScript("scripts/class.lua")
+ScriptHost:LoadScript("scripts/custom_item.lua")
+ScriptHost:LoadScript("scripts/settings.lua")
+Tracker:AddItems("items/items.json")
+Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/broadcast.json")
